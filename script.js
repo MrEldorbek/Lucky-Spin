d1f22973c108850b44dba67c350d6c1d504d2277
MrEldorbek <eldorxusanov05@gmail.com>
2023-03-17 16:11:00 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let container = document.querySelector(".container");
+let btn = document.getElementById("spin");
+let number = Math.ceil(Math.random() * 10000);
+
+btn.onclick = function () {
+  container.style.transform = "rotate(" + number + "deg)";
+  number += Math.ceil(Math.random() * 10000);
+
+}
+  
